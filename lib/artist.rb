e4e73f0b5be1b7b01b5571154d4c80cf87999ab2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-19 17:11:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,18 @@
 class Artist
   attr_reader :name
   @@all = []
-  
+
   def initialize(name)
     @name =name
     @@all << self
   end
-    
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
 end
