9bef24c9899a280cfb0a3da916d2e8f99a155679
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-19 17:05:41 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,18 @@
 class Genre
+  attr_accessor :songs
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+
   end
 end
